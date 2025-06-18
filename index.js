0378e041a3d698ff166e75e1a8f48c076afc7af1
MachdeinBild <lukas.rekowski@machdeinbild.com>
2025-06-18 16:40:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,8 @@ app.post("/generate", async (req, res) => {
     Authorization: `Bearer ${process.env.OPENAI_API_KEY}`
   },
   body: JSON.stringify({
-    model: "dall-e-3",
+    model: "image-alpha-001",
+
     prompt: prompt,
     size: "1024x1024",
     response_format: "url"
